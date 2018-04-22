f8a895a39f92c8dac17fe89d0463f46c137693d6
Jebediah Elliott <jebediahelliott@gmail.com>
2018-04-22 01:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,13 @@ function logWhisper(string)
 }
 function sayHiToGrandam(string)
 {
+  if(string.toLowerCase() === string)
+  {
+    return "I can't hear you!"
+  }
+  if(string.toUpperCase() === string)
+  {
+    return "YES INDEED!"
+  }
   
 }
